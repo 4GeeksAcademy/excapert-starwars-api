a6f7fbddc778e52992a6e62c92a0ad229c88a4bc
eXcapert <130175463+eXcapert@users.noreply.github.com>
2023-10-10 20:31:23 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -17,3 +17,42 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Character(db.Modlel):
+    __table_name__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    birth_year = db.Column(db.String(10), unique=False, nullable=True)
+    eye_color = db.Column(db.String(20), unique=False, nullable=True)
+    gender = db.Column(db.String(20), unique=False, nullable=True)
+    hair_color = db.Column(db.String(20), unique=False, nullable=True)
+    height = db.Column(db.Integer(20), unique=False, nullable=True)
+    mass = db.Column(db.Integer(20), unique=False, nullable=True)
+    skin_color = db.Column(db.String(20), unique=False, nullable=True)
+    url = db.Column(db.String(256), unique=False, nullable=True)
+    created = db.Column(db.DateTime)
+    edited = db.Column(db.DateTime)
+    
+    
+class Planet(db.Model):
+    __tablename__ = "planet"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    diameter = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    gravity = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(120), nullable=False)
+    terrain = db.Column(db.String(120), nullable=False)
+    surface_water = db.Column(db.Integer, nullable=False)
+    url = db.Column(db.String, nullable=False, unique=True)
+    created = db.Column(db.DateTime, nullable=False)
+    edited = db.Column(db.DateTime, nullable=False)
+
+class Favorites(db.Model):
+    __tablename__ = "favorites"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
